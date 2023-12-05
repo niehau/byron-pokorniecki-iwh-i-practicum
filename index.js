4ba1a9a68e9619af4f284e85e8d8097871ec5bdb
Byron Pokorniecki <=>
2023-12-05 20:30:12 +0100

    Implemented CRU + views for Custom Object

--- index.js
+++ index.js
@@ -9,62 +9,108 @@ app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = '';
+const CUSTOM_OBJECT_TYPE_ID = '';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (_, res) => {
+  const customObjectURL = `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT_TYPE_ID}?limit=100&archived=false&properties=dev_id,title,price`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  };
+
+  try {
+    let allResults = [];
+    let hasMore = true;
+    let offset = 0;
+
+    while (hasMore) {
+      const resp = await axios.get(`${customObjectURL}&offset=${offset}`, { headers });
+      const { results, paging } = resp.data;
+
+      allResults = allResults.concat(results);
+
+      if (paging && paging.next) {
+        offset += paging.limit;
+      } else {
+        hasMore = false;
+      }
+    }
+    res.render('index', { title: 'Show Custom Objects and Create Form | Integrating With HubSpot I Practicum', data: allResults });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/update-cobj', async (req, res) => {
+  res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum', dev_id: req.query.dev_id, title: req.query.title, price: req.query.price });
+}
+);
+
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+app.post('/update-cobj', async (req, res) => {
+  const { title, price, dev_id } = req.body;
+  const customObjectBaseURL = `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT_TYPE_ID}`;
+  if (dev_id) {
+    /* the custom property 'dev_id' was setup as unique. Due to this, we dont need the Search API Endpoint */
+    const customObjectURL = `${customObjectBaseURL}/${dev_id}?idProperty=dev_id`;
     const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
+      Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+      'Content-Type': 'application/json'
     };
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
+    const body = {
+      properties: {
+        title,
+        dev_id,
+        price
+      }
+    };
+
+    try {
+      await axios.patch(customObjectURL, body, { headers });
+      res.redirect('/');
+      return;
     }
+    catch (error) {
+      console.error(error);
+      return;
+    }
+  } else {
+    const dev_id = Math.floor(Math.random() * 1000000); // this should actually be a unique ID
+    const customObjectURL = `${customObjectBaseURL}`;
+    const headers = {
+      Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+      'Content-Type': 'application/json'
+    };
+
+    const body = {
+      properties: {
+        title,
+        dev_id,
+        price
+      }
+    };
+
+    try {
+      await axios.post(customObjectURL, body, { headers });
+      res.redirect('/');
+      return;
+    }
+    catch (error) {
+      console.error(error);
+      return;
+    }
+  }
 
 });
-*/
+
 
 
 // * Localhost
